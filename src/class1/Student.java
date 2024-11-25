49ca958ce635102e1c1da3f67d22af6c2a780fea
Yoon Kim <gkrdbsrla@naver.com>
2024-11-25 21:06:33 +0900

    first commit

---
+++ src/class1/Student.java
@@ -0,0 +1,11 @@
+package class1;
+
+// 클래스 : 설계도, 틀(객체를 생성하기 위한)
+// 속성(변수)와 기능(메서드)를 정의
+// 클래스는 관례상 대문자로 시작하고, 낙타 표기법을 사용
+public class Student {
+    // 멤버 변수, 필드
+    String name;
+    int age;
+    int grade;
+}
